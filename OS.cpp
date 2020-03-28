a33be82a2e956102eb645ec060c7b79d3a7bb0f1
Sujit kumar <Singhsujit1999@gmail.com>
2020-03-28 21:26:15 +0530

    Changes in code

--- OS.cpp
+++ OS.cpp
@@ -1,4 +1,4 @@
-#include<iostream.h>
+#include<iostream>
 #include<conio.h>
 using namespace std;
 int main()
@@ -7,22 +7,24 @@ int main()
 	int i,p,q,resource,max_need=0,count=0;
 	cout<<"Enter Number of Resources available : \n";
 	cin>>p;
-	cout<<"Enter Number of Processes available : \n");
+	cout<<"Enter Number of Processes available : \n";
 	cin>>q;
 	resource=p;
 	int process[q],End[q],allot[q];
 	for(int i=0;i<q;i++)
 	{
-		cout<<"Enter number of resources required by Process %d\n"<<i+1;
+		cout<<"Enter number of resources required by Process";
+		cout<<i+1<<"\n";
 		cin>>process[i];
 		End[i]=0;
 		allot[i]=0;
 	}
 	
-	cout<<"\tProcess \tResources\n";
+	cout<<"Process \tResources\n";
 	for(int i=0;i<q;i++)
 	{
-		cout<<"\tP%d \t\t%d\n",i+1,process[i];
+		cout<<i+1;
+		cout<<"\t"<<process[i];
 		max_need=max_need+process[i];
 		if(process[i]<1||process[i]>p)
 		{
@@ -34,7 +36,7 @@ int main()
 		cout<<"\n\n";
 		cout<<"#####";
 		cout<<"\n\n";
-		cout<<"  Both Condition : \n 1) Resource need of each process is between 1 and P \n 2) Max need is less than P+Q.\n\n";
+		cout<<"  Both Condition satisfied: \n";
 	}
 	else
 	{
@@ -45,7 +47,7 @@ int main()
 			count=0;
 			for(int i=0;i<q;i++)
 			{
-				if(finish[i]==0)
+				if(End[i]==0)
 				{
 					if(resource>0)
 					{
@@ -55,11 +57,25 @@ int main()
 					}
 					if(allot[i]==process[i])
 					{
-						cout<<"Process %d completed it execution\n",i+1;
-						finish[i]=1;
+						cout<<"Process completed it execution\n"<<i+1;
+						End[i]=1;
 						resource=resource+process[i];
-						cout<<"Process %d - %d \n",i+1,process[i];
+						cout<<"Process\n";
+						cout<<i+1-process[i];
 					}
 				}
 			}
+			for(int i=0;i<q;i++)
+			{
+				if(End[i]==1)
+				count++;
+			}
+			if(count==q)
+			break;
+		}
+			cout<<"\n";;
+			cout<<"###";
+			cout<<"\n";
+			cout<<"No Dead Lock";
+
 }
